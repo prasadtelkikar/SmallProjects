4652c70b47baa13fc0f481ab1cbcaed026d7f96f
prasadtelkikar <telkikar.prasad@gmail.com>
2017-01-02 17:33:45 +0530

    SmallProject: Extracts all images used by img tag in xsl file and put it into seperate folder

---
+++ ExtractImagesFromXSL/ExtractImagesFromXSL/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ExtractImagesFromXSL
+    {
+    partial class SampleTool
+        {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing)
+            {
+            if ( disposing && (components != null) )
+                {
+                components.Dispose();
+                }
+            base.Dispose(disposing);
+            }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+            {
+            this.lblSelectFolderPath = new System.Windows.Forms.Label();
+            this.txtDisplayFolderPath = new System.Windows.Forms.TextBox();
+            this.browseResultsFolderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnResultsFolder = new System.Windows.Forms.Button();
+            this.btnExtractImages = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSelectFolderPath
+            // 
+            this.lblSelectFolderPath.AutoSize = true;
+            this.lblSelectFolderPath.Location = new System.Drawing.Point(30, 122);
+            this.lblSelectFolderPath.Name = "lblSelectFolderPath";
+            this.lblSelectFolderPath.Size = new System.Drawing.Size(102, 16);
+            this.lblSelectFolderPath.TabIndex = 0;
+            this.lblSelectFolderPath.Text = "Select xsl folder";
+            this.lblSelectFolderPath.Click += new System.EventHandler(this.lblSelectFolderPath_Click);
+            // 
+            // txtDisplayFolderPath
+            // 
+            this.txtDisplayFolderPath.Enabled = false;
+            this.txtDisplayFolderPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDisplayFolderPath.Location = new System.Drawing.Point(167, 116);
+            this.txtDisplayFolderPath.Name = "txtDisplayFolderPath";
+            this.txtDisplayFolderPath.Size = new System.Drawing.Size(553, 26);
+            this.txtDisplayFolderPath.TabIndex = 1;
+            // 
+            // browseResultsFolderDialog
+            // 
+            this.browseResultsFolderDialog.ShowNewFolderButton = false;
+            // 
+            // btnResultsFolder
+            // 
+            this.btnResultsFolder.Location = new System.Drawing.Point(746, 113);
+            this.btnResultsFolder.Name = "btnResultsFolder";
+            this.btnResultsFolder.Size = new System.Drawing.Size(85, 29);
+            this.btnResultsFolder.TabIndex = 2;
+            this.btnResultsFolder.Text = "Browse";
+            this.btnResultsFolder.UseVisualStyleBackColor = true;
+            this.btnResultsFolder.Click += new System.EventHandler(this.btnResultsFolder_Click);
+            // 
+            // btnExtractImages
+            // 
+            this.btnExtractImages.Location = new System.Drawing.Point(345, 179);
+            this.btnExtractImages.Name = "btnExtractImages";
+            this.btnExtractImages.Size = new System.Drawing.Size(112, 35);
+            this.btnExtractImages.TabIndex = 3;
+            this.btnExtractImages.Text = "Run";
+            this.btnExtractImages.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(256, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Extract images from xsl";
+            // 
+            // SampleTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 361);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExtractImages);
+            this.Controls.Add(this.btnResultsFolder);
+            this.Controls.Add(this.txtDisplayFolderPath);
+            this.Controls.Add(this.lblSelectFolderPath);
+            this.MaximumSize = new System.Drawing.Size(900, 400);
+            this.MinimumSize = new System.Drawing.Size(900, 400);
+            this.Name = "SampleTool";
+            this.ShowIcon = false;
+            this.Text = "Extract Images from xsl files";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSelectFolderPath;
+        private System.Windows.Forms.TextBox txtDisplayFolderPath;
+        private System.Windows.Forms.FolderBrowserDialog browseResultsFolderDialog;
+        private System.Windows.Forms.Button btnResultsFolder;
+        private System.Windows.Forms.Button btnExtractImages;
+        private System.Windows.Forms.Label label1;
+        }
+    }
+
---
+++ ExtractImagesFromXSL/ExtractImagesFromXSL/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace ExtractImagesFromXSL
+    {
+    public partial class SampleTool : Form
+        {
+        public SampleTool ()
+            {
+            InitializeComponent();
+            }
+
+        private void lblSelectFolderPath_Click (object sender, EventArgs e)
+            {
+
+            }
+
+        /// <summary>
+        /// Button click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnResultsFolder_Click (object sender, EventArgs e)
+            {
+            Dictionary<string, HashSet<string>> fileWithImages = new Dictionary<string, HashSet<string>>();
+            browseResultsFolderDialog.ShowDialog();
+            string path = browseResultsFolderDialog.SelectedPath;
+
+            if ( string.IsNullOrEmpty(path) )
+                {
+                MessageBox.Show("Please select proper path", "Folder not selected", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                }
+            else
+            {
+                txtDisplayFolderPath.Text = path;
+                fileWithImages = GetImagesWithPath(path);
+                CopyImages(fileWithImages, path);
+                MessageBox.Show(string.Format("Parent folder path: {0}", path), "Completed successfully", MessageBoxButtons.OK);
+                }
+            }
+
+        /// <summary>
+        /// Copy images to destination folder
+        /// </summary>
+        /// <param name="FileWithImages"></param>
+        /// <param name="path"></param>
+        public void CopyImages (Dictionary<string, HashSet<string>> fileWithImages, string path)
+            {
+            foreach ( string fileName in fileWithImages.Keys )
+                {
+                string respFolderPath = Path.Combine(path, Path.GetFileNameWithoutExtension(fileName));
+
+                if ( FolderExists(respFolderPath) )
+                    PurgeFolder(respFolderPath);
+                else
+                    Directory.CreateDirectory(respFolderPath);
+                HashSet<string> imagePaths = fileWithImages[fileName];
+                foreach ( string imgPath in imagePaths )
+                    {
+                    FileInfo srcImg = new FileInfo(imgPath);
+                    if ( File.Exists(srcImg.FullName) )
+                        {
+                        srcImg.CopyTo(Path.Combine(respFolderPath, srcImg.Name), true);
+                        }
+                    }
+                }
+            }
+
+        /// <summary>
+        /// Purge folder
+        /// </summary>
+        /// <param name="respFolderPath"></param>
+        public void PurgeFolder (string respFolderPath)
+            {
+            if ( Directory.EnumerateFileSystemEntries(respFolderPath).Any() )
+                {
+                Directory.Delete(respFolderPath, true);
+                Directory.CreateDirectory(respFolderPath);
+                }
+            }
+
+        /// <summary>
+        /// Folder exists or not
+        /// </summary>
+        /// <param name="respFolderPath"></param>
+        /// <returns></returns>
+        public bool FolderExists (string respFolderPath)
+            {
+            if ( Directory.Exists(respFolderPath) )
+                return true;
+            else
+                return false;
+            }
+
+        /// <summary>
+        /// Get images with full path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public Dictionary<string, HashSet<string>> GetImagesWithPath (string path)
+            {
+            Dictionary<string, HashSet<string>> fileWithImages = new Dictionary<string, HashSet<string>>();
+            var refernceImages = new HashSet<string>();
+
+            if ( Directory.Exists(path) )
+                {
+                DirectoryInfo resultsFolderPath = new DirectoryInfo(path);
+                FileInfo[] xslFiles = resultsFolderPath.GetFiles("*.xsl");
+
+                if ( xslFiles.Length == 0 )
+                    {
+                    MessageBox.Show("Directory does not contains xsl files", "File not found", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                    return null;
+                    }
+
+                else
+                    {
+                    foreach ( FileInfo xslFile in xslFiles )
+                        {
+                        var xsl = XDocument.Load(xslFile.FullName);
+                        var nodesWithSrc = xsl.Root.XPathSelectElements("//*[@src]");
+                        refernceImages = ExtractImages(nodesWithSrc, path);
+                        fileWithImages.Add(xslFile.Name, refernceImages);
+                        }
+                    }
+                return fileWithImages;
+                }
+            else
+                {
+                MessageBox.Show("Directory does not exists", "Path Not found", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return null;
+                }
+            }
+
+        /// <summary>
+        /// Extract images from xsl folder
+        /// </summary>
+        /// <param name="nodesWithSrc"></param>
+        /// <param name="rootFolder"></param>
+        /// <returns></returns>
+        public HashSet<string> ExtractImages (IEnumerable<XElement> nodesWithSrc, string rootFolder)
+            {
+            HashSet<string> imageList = new HashSet<string>();
+            foreach ( var node in nodesWithSrc )
+                {
+                if ( node.Name == "img" )
+                    {
+                    string value = node.Attribute(XName.Get("src")).Value;
+                    string filePath = Path.Combine(rootFolder, value);
+                    if ( File.Exists(filePath) )
+                        {
+                        imageList.Add(filePath);
+                        }
+                    }
+                }
+            return imageList;
+            }
+        }
+    }
